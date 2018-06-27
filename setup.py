4e516058adea9932e330a0c38221d0b2dd0eae66
Bernardo <bperezorozco@gmail.com>
2018-06-27 17:54:05 +0100

    Adds continuous Seq2Seq model; fixed Mordred dropout bug; adds setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ordinal_tsf',
+    version='0.01',
+    packages=['ordinal_tsf'],
+    url='',
+    license='',
+    author='bperezorozco',
+    author_email='ber@robots.ox.ac.uk',
+    description=''
+)
